717ca43525323a5e6457fed50d9d2fed2ebc6287
zoeee97 <1741540959@qq.com>
2020-12-27 16:34:34 +0800

    实现输入框功能

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div class="headline">
       <h1>todos</h1>
-      <input type="text" value="What needs to be done?">
+      <input type="text" v-model="todoValue">
+      <input type="text" :value="todoVal" @input="changeTodoValue">
     </div> 
     <div class="section">
         Double-click to edit a todo<br>
@@ -12,7 +13,29 @@
      <div class="bottom"></div>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      todoValue: "What needs to be done?",
+      todoVal: ""
+    }
+  },
+  methods: {
+    changeTodoValue(newVal) {
+      this.todoVal = newVal
+    }
+  }
+}
+</script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
+h1{
+  color: rgba(228, 175, 159, 0.767);
 
+}
 </style>

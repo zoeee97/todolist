1d6d67a375d1a3a8e34f474f3f607d9d1100d5aa
zoeee97 <1741540959@qq.com>
2020-12-27 15:54:33 +0800

    添加html标签，完成基本框架

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="headline">
+      <h1>todos</h1>
+      <input type="text" value="What needs to be done?">
+    </div> 
+    <div class="section">
+        Double-click to edit a todo<br>
+        Written by Evan You<br>
+        Part of TodoMVC
     </div>
-    <router-view/>
+     <div class="bottom"></div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
